368d61ff775ae0b0a257f80f70474cb49cf3064c
Taco-C <5463191+taco-c@users.noreply.github.com>
2018-12-01 12:55:44 +0100

    Better make wrapper for windows

--- make.bat
+++ make.bat
@@ -1,11 +1,5 @@
-:: Because Windows doesn't have "make".
+:: Wrapper for mingw32-make
 
 @echo off
 
-:: if not exist %binfolder% mkdir %binfolder%
-
-if "%1"=="debug" (
-	mingw32-make debug
-) else (
-	mingw32-make
-)
+mingw32-make %*
